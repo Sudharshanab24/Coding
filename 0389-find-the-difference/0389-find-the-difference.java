69ae36a2e6ee02704984827924677de42929f3e2
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-20 11:37:41 +0530

    Time: 3 ms (44.8%), Space: 42.2 MB (6.9%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,15 +1,26 @@
 class Solution {
     public char findTheDifference(String s, String t) {
-        int[] cnt = new int[26];
-        for (int i = 0; i < s.length(); ++i) {
-            ++cnt[s.charAt(i) - 'a'];
-        }
-        for (int i = 0;i<t.length(); ++i) {
-            if (--cnt[t.charAt(i) - 'a'] < 0) {
-                return t.charAt(i);
+        
+       //MAKING S AND T CHAR ARRAY
+       char arr1[] = s.toCharArray();
+       char arr2[] = t.toCharArray();
+        
+       //SORTING BOTH THE ARRAYS
+       Arrays.sort(arr1);
+       Arrays.sort(arr2);
+        
+        //BY USING FOR LOOP 
+        //IF ANY ELEMENT OF STRING IS DIFFERENT 
+        //RETURN ELEMENT OF ARR2
+        for(int i =0; i<arr1.length; i++)
+        {
+            if(arr1[i] != (arr2[i]))
+            {
+                return arr2[i];
             }
         }
-
-        return ' ';
+        
+        
+        return arr2[arr2.length-1];
     }
 }
