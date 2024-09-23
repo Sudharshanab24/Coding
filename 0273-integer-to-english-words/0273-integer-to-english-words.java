48025f98a66c960ea38cb184c9a27cf4b8258853
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-23 13:46:58 +0530

    Time: 2 ms (90.66%), Space: 41.8 MB (83.27%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.java
+++ 0273-integer-to-english-words/0273-integer-to-english-words.java
@@ -1,31 +1,23 @@
-class Solution {
-  public String numberToWords(int num) {
-    return num == 0 ? "Zero" : helper(num);
-  }
+class Solution
+{
+    private final String[] b20={"","One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen"};
+    private final String[] tens={"","","Twenty","Thirty","Forty","Fifty","Sixty","Seventy","Eighty","Ninety"};
+    public String numberToWords(int num)
+    {
+       return (num==0) ? "Zero":helper(num);
+    }
+    public String helper(int num)
+    {
+        StringBuilder sb=new StringBuilder();
 
-  private final String[] belowTwenty = {"",        "One",     "Two",       "Three",    "Four",
-                                        "Five",    "Six",     "Seven",     "Eight",    "Nine",
-                                        "Ten",     "Eleven",  "Twelve",    "Thirteen", "Fourteen",
-                                        "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"};
-  private final String[] tens = {"",      "",      "Twenty",  "Thirty", "Forty",
-                                 "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"};
+        if(num<20) sb.append(b20[num]);
+        else if(num<100) sb.append(tens[num/10]).append(" ").append(b20[num%10]);
+        else if(num<1000) sb.append(helper(num/100)).append(" Hundred ").append(helper(num%100));
+        else if(num<1000000) sb.append(helper(num/1000)).append(" Thousand ").append(helper(num%1000));
+        else if(num<1000000000) sb.append(helper(num/1000000)).append(" Million ").append(helper(num%1000000));
+        else  sb.append(helper(num/1000000000)).append(" Billion ").append(helper(num%1000000000));
+        
 
-  private String helper(int num) {
-    StringBuilder s = new StringBuilder();
-
-    if (num < 20)
-      s.append(belowTwenty[num]);
-    else if (num < 100)
-      s.append(tens[num / 10]).append(" ").append(belowTwenty[num % 10]);
-    else if (num < 1000)
-      s.append(helper(num / 100)).append(" Hundred ").append(helper(num % 100));
-    else if (num < 1000000)
-      s.append(helper(num / 1000)).append(" Thousand ").append(helper(num % 1000));
-    else if (num < 1000000000)
-      s.append(helper(num / 1000000)).append(" Million ").append(helper(num % 1000000));
-    else
-      s.append(helper(num / 1000000000)).append(" Billion ").append(helper(num % 1000000000));
-
-    return s.toString().trim();
-  }
+        return sb.toString().trim();
+    }
 }
