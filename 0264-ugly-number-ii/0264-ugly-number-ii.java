c4c939f9b231af59d128c2dc117aee9670a84001
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-23 11:45:02 +0530

    Time: 11 ms (46.69%), Space: 43.9 MB (53.75%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.java
+++ 0264-ugly-number-ii/0264-ugly-number-ii.java
@@ -1,44 +1,21 @@
 class Solution {
     public int nthUglyNumber(int n) {
-        // Initialize a priority queue (min-heap) to store the ugly numbers
-        PriorityQueue<Long> pq = new PriorityQueue<>();
-        // Use a hash set to avoid duplicates
-        HashSet<Long> seen = new HashSet<>();
+        int[] num = new int[1690];
+        num[0] = 1;
+        int ugly = 0, i2 = 0, i3 = 0, i5 = 0;
 
-        // Start with 1 as the first ugly number
-        pq.offer(1L);
-        seen.add(1L);
+        for (int i = 1; i < 1690; i++) {
+            num[i] = Math.min(Math.min(2 * num[i2], 3 * num[i3]), 5 * num[i5]);
+            ugly = num[i];
 
-        int count = 0;
-        long currentUgly = 1;
-
-        while (count < n) {
-            currentUgly = pq.poll();
-            count++;
-
-            if (count == n) {
-                return (int) currentUgly;
-            }
-
-            long x1 = currentUgly * 2;
-            long x2 = currentUgly * 3;
-            long x3 = currentUgly * 5;
-
-            // Add the new numbers to the heap if they haven't been added before
-            if (!seen.contains(x1)) {
-                pq.offer(x1);
-                seen.add(x1);
-            }
-            if (!seen.contains(x2)) {
-                pq.offer(x2);
-                seen.add(x2);
-            }
-            if (!seen.contains(x3)) {
-                pq.offer(x3);
-                seen.add(x3);
-            }
+            if (num[i2] * 2 == ugly)
+                i2++;
+            if (num[i3] * 3 == ugly)
+                i3++;
+            if (num[i5] * 5 == ugly)
+                i5++;
         }
-        return 0;
 
+        return num[n - 1];
     }
 }
