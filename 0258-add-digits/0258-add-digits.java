b46873bca688d716d68f02f48f09481ec28661dd
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-23 09:12:02 +0530

    Time: 1 ms (52.32%), Space: 40.4 MB (97.18%) - LeetHub

--- 0258-add-digits/0258-add-digits.java
+++ 0258-add-digits/0258-add-digits.java
@@ -1,17 +1,9 @@
 class Solution {
-    public int addDigits(int num) 
-    {
-        if(num<10) return num;
-        
-        int sum=0;
-        int a=num;
-        while(a!=0)
-        {
-            int rem=a%10;
-            sum=sum+rem;
-            a=a/10;
-        }
-        
-        return addDigits(sum);
+    public int addDigits(int num) {
+        // if num is zero then return
+        if(num == 0) return 0;
+        // if num is rather  than zero modulo it with 9 bcoz the sum of
+        // two num is going only to nine then come up to its module with nine only.
+        return num%9 == 0 ? 9 : num%9;
     }
 }
