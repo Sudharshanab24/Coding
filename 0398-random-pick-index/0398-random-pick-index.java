be67539bf9dc45a640dad77919ec975abbf9eafe
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-23 10:04:41 +0530

    Time: 1011 ms (27.42%), Space: 55.5 MB (76.4%) - LeetHub

--- 0398-random-pick-index/0398-random-pick-index.java
+++ 0398-random-pick-index/0398-random-pick-index.java
@@ -10,26 +10,36 @@ class Solution {
     
     public int pick(int target)
     {
-        int count=0;
-        int result=0;
+        // int count=0;
+        // int result=0;
+
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]==target)
+        //     {
+        //         count++;
+
+        //         int randomNumber=1+random.nextInt(count);
+
+        //         if(randomNumber==count) result=i;
+        //     }
+
+        // }
+
+        // return result;
+
+        ArrayList<Integer> a=new ArrayList<>();
 
         for(int i=0;i<nums.length;i++)
         {
             if(nums[i]==target)
-            {
-                count++;
-
-                int randomNumber=1+random.nextInt(count);
-
-                if(randomNumber==count) result=i;
-            }
-
+            a.add(i);
         }
 
-        return result;
+        return a.get(random.nextInt(a.size()));
     }
 }
-
+   
 /**
  * Your Solution object will be instantiated and called as such:
  * Solution obj = new Solution(nums);
