5ccdba0589c3492c569914259b5d583afa072625
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-24 11:37:18 +0530

    Time: 0 ms (100%), Space: 42.7 MB (6.42%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -4,33 +4,33 @@ class Solution {
         int m=matrix.length;
         int n=matrix[0].length;
 
-        // for(int i=0;i<m;i++)
-        // {
-        //     if(matrix[i][0]<=target)
-        //     {
-        //         for(int j=0;j<n;j++)
-        //         {
-        //             if(matrix[i][j]==target) return true;
-        //         }
-        //     }
-        // }
-
-        // return false;
-
-        int l=0;
-        int r=m*n;
-
-        while(l<r)
+        for(int i=0;i<m;i++)
         {
-            final int mid=(l+r)/2;
-            final int i=mid/n;
-            final int j=mid%n;
-
-            if(matrix[i][j]==target) return true;
-            if(matrix[i][j]<target) l=mid+1;
-            else r=mid;
+            if(matrix[i][0]<=target)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    if(matrix[i][j]==target) return true;
+                }
+            }
         }
 
         return false;
+
+        // int l=0;
+        // int r=m*n;
+
+        // while(l<r)
+        // {
+        //     final int mid=(l+r)/2;
+        //     final int i=mid/n;
+        //     final int j=mid%n;
+
+        //     if(matrix[i][j]==target) return true;
+        //     if(matrix[i][j]<target) l=mid+1;
+        //     else r=mid;
+        // }
+
+        // return false;
     }
 }
