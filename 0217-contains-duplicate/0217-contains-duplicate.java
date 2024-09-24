cb86b2cd808009350b4c80333a02b82e3ab4a0eb
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-24 10:11:04 +0530

    Time: 17 ms (40.4%), Space: 58.3 MB (30.27%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,25 +1,25 @@
-// class Solution {
-//     public boolean containsDuplicate(int[] nums)
-//     {
-//         HashSet<Integer> hs=new HashSet<>();
-
-//         for(int i=0;i<nums.length;i++)
-//         hs.add(nums[i]);
-
-//         if(hs.size()!=nums.length) return true;
-//         return false;
-//     }
-// }
-
 class Solution {
-    public boolean containsDuplicate(int[] nums) {
-        Arrays.sort(nums); 
-        for (int i = 0; i < nums.length - 1; i++) {
-                if (nums[i] == nums[i+1]) {
-                    return true;
-                }
-        }
+    public boolean containsDuplicate(int[] nums)
+    {
+        HashSet<Integer> hs=new HashSet<>();
+
+        for(int i=0;i<nums.length;i++)
+        hs.add(nums[i]);
+
+        if(hs.size()!=nums.length) return true;
         return false;
     }
 }
+
+// class Solution {
+//     public boolean containsDuplicate(int[] nums) {
+//         Arrays.sort(nums); 
+//         for (int i = 0; i < nums.length - 1; i++) {
+//                 if (nums[i] == nums[i+1]) {
+//                     return true;
+//                 }
+//         }
+//         return false;
+//     }
+// }
 
