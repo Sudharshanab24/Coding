798ca8c82091185ca1d1b90a677769203fa7d90a
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-24 13:36:38 +0530

    Time: 5 ms (25.28%), Space: 45.7 MB (8.05%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,16 @@
+class Solution
+{
+    public int missingNumber(int[] nums)
+    {
+        Arrays.sort(nums);
+
+        int n=nums.length;int i=0;
+
+        for(i=0;i<n;i++)
+        {
+        if(nums[i]!=i)
+        return i;
+        }
+        return n;
+    }
+}
