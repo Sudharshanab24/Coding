79a1e48d238c5de3b629f731614fe69dc718737d
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-09-26 10:25:47 +0530

    Time: 7 ms (100%), Space: 49.7 MB (14.18%) - LeetHub

--- 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
+++ 0303-range-sum-query-immutable/0303-range-sum-query-immutable.java
@@ -1,18 +1,25 @@
 class NumArray {
 
-    private int[] nums;
+    private int[] pre;
 
     public NumArray(int[] nums)
     {
-        this.nums=nums;
+        // this.nums=nums;
+        pre=new int[nums.length+1];
+
+        for(int i=0;i<nums.length;i++)
+        pre[i+1]=pre[i]+nums[i];
+
     }
     
     public int sumRange(int left, int right)
     {
-        int sum=0;
-        for(int i=left;i<=right;i++)
-        sum=sum+nums[i];
-        return sum;
+        // int sum=0;
+        // for(int i=left;i<=right;i++)
+        // sum=sum+nums[i];
+        // return sum;
+
+        return pre[right+1]-pre[left];
     }
 }
 
