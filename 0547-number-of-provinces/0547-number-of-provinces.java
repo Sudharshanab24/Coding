7a0a6c9ed0e8c53226f92bb89b69058472cb3804
Sudharshanab24 <126388814+Sudharshanab24@users.noreply.github.com>
2024-11-05 15:33:33 +0530

    Time: 2 ms (54.4%), Space: 47.4 MB (66.44%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -20,7 +20,7 @@ class Solution
 
         for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<=i;j++)
             {
                 if(isConnected[i][j]==1 && i!=j)
                 {
