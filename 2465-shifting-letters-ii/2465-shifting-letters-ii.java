a36ff5cef5873f6e00627b26f6d45f6bcd4e3806
Sudharshana B <126388814+Sudharshanab24@users.noreply.github.com>
2025-01-05 19:40:20 +0530

    Time: 9 ms (26.89%), Space: 74.6 MB (46.23%) - LeetHub

--- 2465-shifting-letters-ii/2465-shifting-letters-ii.java
+++ 2465-shifting-letters-ii/2465-shifting-letters-ii.java
@@ -1,24 +1,28 @@
 class Solution {
-  public String shiftingLetters(String s, int[][] shifts) {
-    StringBuilder sb = new StringBuilder();
-    int currShift = 0;
-    int[] timeline = new int[s.length() + 1];
+    public String shiftingLetters(String s, int[][] shifts) {
+        
+        StringBuilder sb=new StringBuilder();
+        int curr=0;
+        int tl[]=new int[s.length()+1];
 
-    for (int[] shift : shifts) {
-      final int start = shift[0];
-      final int end = shift[1];
-      final int direction = shift[2];
-      final int diff = direction == 1 ? 1 : -1;
-      timeline[start] += diff;
-      timeline[end + 1] -= diff;
+        for(int i=0;i<shifts.length;i++)
+        {
+            int st=shifts[i][0];
+            int e=shifts[i][1];
+            int d=shifts[i][2];
+
+            int diff=(d==1)?1:-1;
+
+            tl[st]+=diff;
+            tl[e+1]-=diff;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            curr=(curr+tl[i])%26;
+            int n=(s.charAt(i)-'a'+curr+26)%26;
+            sb.append((char)('a'+n));
+        }
+
+        return sb.toString();
     }
-
-    for (int i = 0; i < s.length(); ++i) {
-      currShift = (currShift + timeline[i]) % 26;
-      final int num = (s.charAt(i) - 'a' + currShift + 26) % 26;
-      sb.append((char) ('a' + num));
-    }
-
-    return sb.toString();
-  }
 }
