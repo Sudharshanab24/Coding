279cb3053db82a89b760acf8d29948f5b48636f7
Sudharshana B <126388814+Sudharshanab24@users.noreply.github.com>
2025-01-20 23:04:08 +0530

    Time: 0 ms (100%), Space: 42.3 MB (38.97%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -3,9 +3,9 @@ class Solution {
     {
         int n=matrix.length;
 
-        for(int i=0;i<n;i++)
+        for(int i=0;i<n-1;i++)
         {
-            for(int j=i;j<n;j++)
+            for(int j=i+1;j<n;j++)
             {
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
