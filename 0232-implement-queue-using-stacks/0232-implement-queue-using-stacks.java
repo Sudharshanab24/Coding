6be7b8bce4c099c99983ced21b5beda6fc713b14
Sudharshana B <126388814+Sudharshanab24@users.noreply.github.com>
2025-01-28 15:51:21 +0530

    Time: 0 ms (100%), Space: 41.7 MB (19.15%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,48 +1,49 @@
-import java.util.Stack;
-
 class MyQueue {
 
-    Stack<Integer> st1;
-    Stack<Integer> st2;
+    Stack <Integer> st1;
+    Stack <Integer> st2;
 
     public MyQueue() {
-        st1 = new Stack<>();
-        st2 = new Stack<>();
+        st1=new Stack<>();
+        st2=new Stack<>();
     }
     
-    // Push element x to the back of the queue
     public void push(int x) {
-        // Push to st1 directly
         st1.push(x);
     }
     
-    // Remove the front element of the queue
     public int pop() {
-        // Transfer elements from st1 to st2 if st2 is empty
-        if (st2.isEmpty()) {
-            while (!st1.isEmpty()) {
+        if(st2.isEmpty())
+        {
+            while(!st1.isEmpty())
+            {
                 st2.push(st1.pop());
             }
         }
-        // Pop from st2
         return st2.pop();
     }
     
-    // Get the front element of the queue
     public int peek() {
-        // Transfer elements from st1 to st2 if st2 is empty
-        if (st2.isEmpty()) {
-            while (!st1.isEmpty()) {
+        if(st2.isEmpty())
+        {
+            while(!st1.isEmpty())
+            {
                 st2.push(st1.pop());
             }
         }
-        // Peek at the front element of st2
         return st2.peek();
     }
     
-    // Check if the queue is empty
     public boolean empty() {
-        // Queue is empty if both stacks are empty
-        return st1.isEmpty() && st2.isEmpty();
+        return st2.isEmpty() && st1.isEmpty();
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
