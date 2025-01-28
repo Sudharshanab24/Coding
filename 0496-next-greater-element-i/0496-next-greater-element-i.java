911f4170351e17a667edfe53165ae032f0cfe314
Sudharshana B <126388814+Sudharshanab24@users.noreply.github.com>
2025-01-28 18:57:27 +0530

    Time: 4 ms (41.84%), Space: 44.8 MB (8.29%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,54 +1,26 @@
 class Solution {
-    public int[] nextGreaterElement(int[] nums1, int[] nums2)
-    {
-    //     HashMap<Integer,Integer> hm=new HashMap<>();
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
 
-    //     for(int i=0;i<nums2.length;i++)
-    //         hm.put(nums2[i],i);
-        
-    //     for(int i=0;i<nums1.length;i++)
-    //     {
-    //         int j=hm.get(nums1[i]);
-    //         int k=0;int found=0;
-            
-    //         for(k=j+1;k<nums2.length;k++)
-    //         {
-    //             if(nums2[j]<nums2[k])
-    //             {
-    //             System.out.println(nums2[k]);
-    //             nums1[i]=nums2[k];
-    //             found=1;
-    //             break;
-    //             }
-    //         }
-    //         if(found==0) nums1[i]=-1;
-    //     }
+        Stack<Integer> st=new Stack<>();
+        HashMap<Integer,Integer> hm=new HashMap<>();
 
-    // return nums1;
-    // }
+        for(int i=0;i<nums2.length;i++)
+        {
+            while(!st.isEmpty() && st.peek()<nums2[i])
+            hm.put(st.pop(),nums2[i]);
 
-    List<Integer> ans=new ArrayList<>();
-    Map<Integer,Integer>hm=new HashMap<>();
-    Deque<Integer> st=new ArrayDeque<>();
+            st.push(nums2[i]);
+        }
 
-    for(int num:nums2)
-    {
-        while(!st.isEmpty() && st.peek()<num)
-        hm.put(st.pop(),num);
+        for(int i=0;i<nums1.length;i++)
+        {
+            if(hm.containsKey(nums1[i]))
+            nums1[i]=hm.get(nums1[i]);
 
-        st.push(num);
-    }
+            else
+            nums1[i]=-1;
+        }
 
-    for(int num:nums1)
-    {
-        if(hm.containsKey(num))
-            ans.add(hm.get(num));
-        
-        else 
-        ans.add(-1);
-    }
-
-    return ans.stream().mapToInt(Integer::intValue).toArray();
-
+        return nums1;
     }
 }
