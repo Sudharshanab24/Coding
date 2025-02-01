cd7109973a6e56a9cb1ec8d0697b05e9f9b4fc6f
Sudharshana B <126388814+Sudharshanab24@users.noreply.github.com>
2025-02-01 19:18:40 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.68%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -3,8 +3,10 @@ class Solution {
 
         for(int i=0;i<nums.length-1;i++)
         {
-           if(nums[i]%2==0 && nums[i+1]%2==0) return false;
-           if(nums[i]%2!=0 && nums[i+1]%2!=0) return false;
+        //    if(nums[i]%2==0 && nums[i+1]%2==0) return false;
+        //    if(nums[i]%2!=0 && nums[i+1]%2!=0) return false;
+
+        if(nums[i]%2==nums[i+1]%2) return false;
         }
 
         return true;
