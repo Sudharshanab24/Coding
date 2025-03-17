c450842f6113a198e66c4019f791939f95660914
Sudharshana B <sudhubalu04@gmail.com>
2025-03-17 19:39:21 +0530

    Time: 8 ms (40.3%), Space: 44.2 MB (85.16%) - LeetHub

--- 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -1,10 +1,30 @@
 class Solution {
-  public boolean divideArray(int[] nums) {
-    int[] count = new int[501];
+    public boolean divideArray(int[] nums) {
 
-    for (final int num : nums)
-      ++count[num];
+        int m=nums.length;
+        HashMap<Integer,Integer>hm=new HashMap<>();
 
-    return Arrays.stream(count).allMatch(c -> c % 2 == 0);
-  }
+        for(int i=0;i<m;i++)
+            hm.put(nums[i],hm.getOrDefault(nums[i],0)+1);
+        
+        for(int value:hm.values())
+        {
+            if(value%2!=0) return false;
+        }
+
+        return true;
+        
+    }
 }
+
+
+// class Solution {
+//   public boolean divideArray(int[] nums) {
+//     int[] count = new int[501];
+
+//     for (final int num : nums)
+//       ++count[num];
+
+//     return Arrays.stream(count).allMatch(c -> c % 2 == 0);
+//   }
+// }
