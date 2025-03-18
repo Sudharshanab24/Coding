c2e75d4674b41e13d81f1c8b6f3642c878b02f8f
Sudharshana B <sudhubalu04@gmail.com>
2025-03-18 22:19:24 +0530

    Time: 51 ms (54.55%), Space: 58.1 MB (54.03%) - LeetHub

---
+++ 1604-least-number-of-unique-integers-after-k-removals/1604-least-number-of-unique-integers-after-k-removals.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findLeastNumOfUniqueInts(int[] arr, int k) {
+
+        HashMap<Integer,Integer> hm=new HashMap<>();
+
+        for(int i=0;i<arr.length;i++)
+        hm.put(arr[i],hm.getOrDefault(arr[i],0)+1);
+
+        PriorityQueue<Integer> mh=new PriorityQueue<>(hm.values());
+
+        while(k>0)
+            k=k-mh.poll();
+        
+
+        return mh.size()+(k<0 ? 1:0);
+        
+    }
+}
